3434c572a4524bc84f74a43e20ed58be8f28e0de
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-07-04 20:28:22 -0600

    ✨  add env

--- src/config.js
+++ src/config.js
@@ -3,3 +3,8 @@ import { config } from 'dotenv'
 config()
 
 export const PORT = process.env.PORT || '5000'
+export const DB_HOST = process.env.DB_HOST || '127.0.0.1'
+export const DB_USER = process.env.DB_USER || 'arzcorp'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'Osva0807$'
+export const DB_PORT = process.env.DB_PORT || '3306'
+export const DB_NAME = process.env.DB_NAME || 'quiniela_app'
