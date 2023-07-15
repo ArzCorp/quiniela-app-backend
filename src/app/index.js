64f8ad3dbbd7b2c0d2f991b0a5eb6f81942a9355
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-07-14 22:33:40 -0600

    ✨ add cors

--- src/app/index.js
+++ src/app/index.js
@@ -3,9 +3,11 @@ import { homeRouter } from '../routes/home.routes.js'
 import { notFoundController } from '../controllers/404.controller.js'
 import { LogInRouter } from '../routes/login.routes.js'
 import { registerRouter } from '../routes/register.routes.js'
+import cors from 'cors'
 
 export const app = express()
 
+app.use(cors())
 app.use(json())
 
 app.use(homeRouter)
